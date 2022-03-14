32aa5af8a9a7289cb881ed8d7d9f0000ff8567b9
Victor Pereira <victor.m.s.pereira.95@gmail.com>
2022-03-14 12:59:12 -0300

    Correção de bugs

--- src/dados/Calculadora.java
+++ src/dados/Calculadora.java
@@ -29,45 +29,13 @@ public class Calculadora {
 		T menor = list.get(0);
 
 		for (T j : list) {
-			if (j.compareTo(menor) < 0) {
+			if (j.compareTo(menor) > 0) {
 				menor = j;
 			}
 		}
 		return menor;
 	}
 	
-	public static Integer padraoMaior(List<Integer> list) {
 
-		if (list.isEmpty()) {
-			throw new IllegalStateException("Erro: a lista está vazia");
-		}
-
-		Integer padraoMaior = list.get(0);
-		
-		for (Integer i : list) {
-			if (i > list.get(0)) {
-				padraoMaior += i;
-			}
-		}
-		return padraoMaior;
-	}
-
-	public static Integer padraoMenor(List<Integer> list) {
-
-		if (list.isEmpty()) {
-			throw new IllegalStateException("Erro: a lista está vazia");
-		}
-
-		Integer padraoMenor = list.get(0);
-
-		for (Integer j : list) {
-			if (j < list.get(0)) {
-				padraoMenor -= j;
-			}
-		}
-		return padraoMenor;
-	}
-
-	
 }
 
--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -12,9 +12,8 @@ import times.Times;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		List<Integer>lista = new ArrayList();
+		List<Times>lista = new ArrayList<>();
 		
 		String arquivo = "c:\\hacker\\br16.txt";
 		
@@ -22,23 +21,18 @@ public class Programa {
 			
 			String linha = br.readLine();
 			while (linha != null) {
-				String[] campos = linha.split("	");
-				lista.add(new Times(campos[1], Integer.parseInt(campos[2]), Integer.parseInt(campos[3]), Integer.parseInt(campos[4]), Integer.parseInt(campos[5]), Integer.parseInt(campos[6]), Integer.parseInt(campos[7]), Integer.parseInt(campos[8])));
+				String[] campos = linha.split(" ");
+				lista.add(new Times(campos[0], Integer.parseInt(campos[1])));
 				linha = br.readLine();
 			}
 			
-			Integer x = Calculadora.maior(lista);
-			Integer y = Calculadora.menor(lista);
-			Integer j = Calculadora.padraoMaior(lista);
-			Integer k = Calculadora.padraoMenor(lista);
+			Times x = Calculadora.maior(lista);
+			Times y = Calculadora.menor(lista);
 			System.out.print("Maior: ");
 			System.out.println(x);
 			System.out.print("Menor: ");
 			System.out.println(y);
-			System.out.print("Padrão Maior: ");
-			System.out.println(j);
-			System.out.print("Padrão Menor: ");
-			System.out.println(k);
+
 		}
 		
 		catch (IOException e) {
